095c19998e02daf5a01602ff9a9aba251459aed9
Sergey Butuzov <sergey.butuzov@idp.zyfra.com>
2024-09-05 13:01:01 +0400

    add eslint

--- src/app/todo/store/todo.actions.ts
+++ src/app/todo/store/todo.actions.ts
@@ -1,5 +1,4 @@
 import { createAction, props } from '@ngrx/store';
-import { TodoModel } from '../models';
 
 export const check = createAction('[TodoList Component] Check', props<{ todoId: string }>());
 export const uncheck = createAction('[TodoList Component] Uncheck', props<{ todoId: string }>());
